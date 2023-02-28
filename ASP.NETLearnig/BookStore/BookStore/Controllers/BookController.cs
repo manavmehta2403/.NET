cc75a36201f9a0a0af7d7d0e5568d18c79fc24e7
Manav Mehta <manav.mehta@wuh-group.com>
2023-02-28 16:15:46 +0530

    OK for now

---
+++ ASP.NETLearnig/BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,33 @@
+﻿using BookStore.Models;
+using BookStore.Repositary;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+  public class BookController : Controller
+  {
+
+    private readonly BookRespositary _bookRespositary;
+
+    public BookController() 
+    {
+      _bookRespositary = new BookRespositary();
+    } 
+    public ViewResult GetAllBooks()
+    {
+      var data = _bookRespositary.GetAllBooks();
+
+      return View();
+    }
+
+    public BookModel GetBook(int id)
+    {
+      return _bookRespositary.GetBook(id);
+    }
+
+    public List<BookModel> SearchBooks(string book, string author)
+    {
+      return _bookRespositary.SearchBook(book,author);
+    }
+  }
+}
---
+++ ASP.NETLearnig/BookStore/BookStore/Repositary/BookRespositary.cs
@@ -0,0 +1,39 @@
+﻿using BookStore.Models;
+
+namespace BookStore.Repositary
+{
+  public class BookRespositary
+  {
+    public List<BookModel> GetAllBooks()
+    {
+      return DataSource();
+    }
+    public BookModel GetBook(int id)
+    {
+      return DataSource().FirstOrDefault(x => x.Id == id);
+    }
+    //public void UpdateBook(BookModel book) 
+    //{ 
+
+    //}
+    //public void DeleteBook(int id) 
+    //{
+      
+    //}
+    public List<BookModel> SearchBook(string book, string author)
+    {
+      return DataSource().Where(x => x.Title.Contains(book) || x.Author.Contains(author)).ToList();
+    }
+
+    private List<BookModel> DataSource() 
+    {
+      return new List<BookModel>()
+      {
+        new BookModel() {Id = 1, Title="MVC" ,Author = "Raja"},
+        new BookModel() {Id = 2, Title="MVVM" ,Author = "Saja"},
+        new BookModel() {Id = 3, Title="WebApp" ,Author = "Baja"},
+        new BookModel() {Id = 4, Title="OSApp" ,Author = "Taja"},
+      };
+    }
+  }
+}

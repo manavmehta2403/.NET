25613df7e38e86c9268e5b294297106cdb03f1d6
Manav Mehta <Manav.Mehta@wuh-group.com>
2023-03-23 17:38:44 +0530

    Dumb error resolve

--- ButtonCustomTriggering/ButtonCustomControl/obj/Debug/netcoreapp3.1/MainMindow.g.i.cs
+++ ButtonCustomTriggering/ButtonCustomControl/obj/Debug/netcoreapp3.1/MainMindow.g.i.cs
@@ -55,13 +55,13 @@ namespace ButtonCustomControl {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ButtonCustomControl;component/mainmindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ButtonCustomControl;V1.0.0.0;component/mainmindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainMindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -71,7 +71,7 @@ namespace ButtonCustomControl {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
